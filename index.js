c3296284aa7b87f1af809cf6585e819a075ffeb1
leandro.lopes <leandro.lopes@premmiar.com.br>
2022-02-21 17:22:50 -0300

    FIX

--- index.js
+++ index.js
@@ -1,109 +1,59 @@
 const tesseract = require("node-tesseract-ocr")
-const {pdf} = require("pdf-to-img")
+const { pdf } = require("pdf-to-img")
 const fs = require('fs')
 const moveFrom = "./PDFs";
 const moveTo = "./images";
-const path = require( 'path' );
+const path = require('path');
 const config = {
   lang: "POR",
   oem: 1,
   psm: 3,
 }
+init()
+async function init(){
 
-/*async function init() {
-  
-  const image = await pdf("./PDFs/slide.pdf",{scale:15})
-      console.log(image)
-          for await (const page of image) {
-              console.log(page)
-              fs.writeFile('file.png',page, callback);
-  }
-          await tesseract
-                  .recognize('./file.png', config)
-                  .then((text) => {
-                    console.log(text)
-          })
-  .catch((error) => {
-            console.log(error.message)
+  const directoryPath = path.join(__dirname, 'PDFs');
+
+  fs.readdir(directoryPath, async function (err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    for (let index = 0; index < files.length; index++) {
+      const element = files[index];
+      await   convert(element)
+      
+    }
+
   })
 }
-init()
 
-var callback = (err) => {
-          if (err) throw err;
-          console.log('It\'s saved!');
+
+async function convert(file) {
+  const image = await pdf('./PDFs/' + file, { scale: 15 })
+  const fileImagName = file.replace('.pdf','.png')
+  console.log('IMAGE == M',file)
+
+let passou = false;
+  for await (const page of image) {
+    if(!passou ){
+      passou = true 
+      console.log('pAGE', image[page])
+      fs.writeFile('./images/' + fileImagName, page, callback);
+    }
+  }
+  setTimeout(async ()=> {
+    await tesseract
+    .recognize('./images/' + fileImagName, config)
+    .then((text) => {
+      console.log(text)
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
+  },500)
 }
 
-
-async function init(){
-  try {
-      const files = await fs.promises.readdir( moveFrom );
-
-      for( const file of files ) {
-          const fromPath = path.join( moveFrom, file );
-          const convertTo = path.join( convertTo, file );
-          const stat = await fs.promises.stat( fromPath );
-
-          if( stat.isFile() )
-              console.log( "'%s' is a file.", fromPath );
-          else if( stat.isDirectory() )
-              console.log( "'%s' is a directory.", fromPath );          
-  
-            const image = await pdf(fromPath,{scale:15})
-                console.log(image)
-                    for await (const page of image) {
-                        console.log(page)
-                        fs.writeFile(convertTo,page, callback);
-            }
-                    await tesseract
-                            .recognize(convertTo, config)
-                            .then((text) => {
-                              console.log(text)
-                    })
-            .catch((error) => {
-                      console.log(error.message)
-            })
-          }
-          init()
-
-          var callback = (err) => {
-                    if (err) throw err;
-                    console.log('It\'s saved!');
-            }
-      }catch(err){
-        console.log(err);
-      }} */
-
-
-const directoryPath = path.join(__dirname, 'PDFs');
-async function init() {  
-fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-     files.forEach(function (file) {
-
-          const image = await pdf('./PDFs/' + file,{scale:15})
-              console.log(image)
-                  for await (const page of image) {
-                      console.log(page)
-                      fs.writeFile('./images/'+ image ,page, callback);
-          }
-                  await tesseract
-                          .recognize('./images/'+ image, config)
-                          .then((text) => {
-                            console.log(text)
-                  })
-          .catch((error) => {
-                    console.log(error.message)
-          })
-        
-
-    });
-})}
-init()
-        
 var callback = (err) => {
-          if (err) throw err;
-          console.log('It\'s saved!');
+  if (err) throw err;
+  console.log('It\'s saved!');
 }
